f5a2da49ccfe18cbd4fe076d88b9a694fd7b063b
Saurabh <saurabhrohankar99@gmail.com>
2023-08-31 16:26:24 +0530

    error handling shell scripts

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,10 +4,14 @@ set -e
 
 echo "Stopping and removing running containers..."
 
-# Stop and remove containers, if any
-if docker ps -a -q >/dev/null 2>&1; then
-    docker stop $(docker ps -a -q) || true  # Stop containers, ignoring errors
-    docker rm -f $(docker ps -a -q) || true  # Remove containers, ignoring errors
+# Get a list of running containers
+CONTAINERS_TO_STOP=$(docker ps -a -q)
+
+if [ -n "$CONTAINERS_TO_STOP" ]; then
+    docker stop $CONTAINERS_TO_STOP || true  # Stop containers, ignoring errors
+    docker rm -f $CONTAINERS_TO_STOP || true  # Remove containers, ignoring errors
+else
+    echo "No running containers to stop and remove."
 fi
 
 echo "Containers stopped and removed successfully."
